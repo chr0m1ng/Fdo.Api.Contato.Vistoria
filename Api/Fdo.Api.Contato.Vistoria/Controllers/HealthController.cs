f0da9622b50f2d390dd05a7802904711c39e9b3c
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-10-14 22:39:17 -0300

    Add: health check controller

---
+++ Api/Fdo.Api.Contato.Vistoria/Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fdo.Api.Contato.Vistoria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        /// <summary>
+        /// Ping endpoint
+        /// </summary>
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Alive!");
+        }
+    }
+}
