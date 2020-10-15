69fbc9ce2a9ee6a3e9836965a6f1ffea1f8d33e7
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-10-14 22:40:16 -0300

    Add: patch config model; Remove: unused constant

--- Api/Fdo.Api.Contato.Vistoria.Models/Constants.cs
+++ Api/Fdo.Api.Contato.Vistoria.Models/Constants.cs
@@ -4,5 +4,6 @@
     {
         public const string XML_EXTENSION = ".xml";
         public const string PROJECT_NAME = "Fdo.Api.Contato.Vistoria";
+        public const string UI_PROJECT_NAME = "Fdo.Api.Contato.Vistoria.UI";
     }
 }
---
+++ Api/Fdo.Api.Contato.Vistoria.Models/Requests/Config/ConfigPatchRequest.cs
@@ -0,0 +1,11 @@
+﻿
+using Newtonsoft.Json;
+
+namespace Fdo.Api.Contato.Vistoria.Models.Requests.Config
+{
+    public class ConfigPatchRequest
+    {
+        [JsonProperty("storage_path")]
+        public string StoragePath { get; set; }
+    }
+}
